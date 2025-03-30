1773d569e7cc27e3cbd6b0e55222eefd53198484
ClaytonSeabaughGH <claytonseabaugh@gmail.com>
2025-03-29 23:53:23 -0500

    Changing Case in a string with Methods

---
+++ python_work/name.py
@@ -0,0 +1,7 @@
+# This introduces title() method changes the word to title case where each word begins with a capital
+name = "ada lovelace"
+print(name.title())
+
+name = "Ada Lovelace"
+print(name.upper())
+print(name.lower())
