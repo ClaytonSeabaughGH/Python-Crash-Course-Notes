2173397bdf7a84136f091617987c9737379d8653
ClaytonSeabaughGH <claytonseabaugh@gmail.com>
2025-03-29 23:49:20 -0500

    Finished up Variables TIY

---
+++ python_work/simple_messages.py
@@ -0,0 +1,7 @@
+# Assign a message to a variable and then print
+name = "Clayton Seabaugh"
+print(name)
+
+# Assign a message to a variable, print that message, change variable, then print again
+favorite_number= "Four"
+print(favorite_number)
