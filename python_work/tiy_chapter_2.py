0903208e5861b09c8e89041f275edec952530566
ClaytonSeabaughGH <claytonseabaugh@gmail.com>
2025-04-01 23:41:04 -0500

    Finish TIY with chapter 2

--- python_work/tiy_chapter_2.py
+++ python_work/tiy_chapter_2.py
@@ -8,7 +8,17 @@ print(my_name.title())
 print(my_name.lower())
 print(my_name.upper())
 
-# 2-5 Find a quote, print the quote and name of author. 
+# 2-5 and 2-6 Find a quote, print the quote and name of author. 
 author = "René Descartes"
 quote = '"I think, therefore I am"'
 print(f'{author} once said, {quote}')
+
+# 2-7 Use a variable to represent a person's name with white space. Practice lstrip, rstrip, and strip
+random_name = " Jerry "
+print(random_name.lstrip())
+print(f'Name: \t {random_name.rstrip()}')
+print(f'Name: \n{random_name.strip()}')
+
+# 2-8 Use removesuffix 
+filename = 'python_notes.txt'
+print(filename.removesuffix('.txt'))
