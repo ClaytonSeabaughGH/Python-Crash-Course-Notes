c25edc0dc7646f6a9a23ffc04570aaf3c2ea30a9
ClaytonSeabaughGH <claytonseabaugh@gmail.com>
2025-04-01 23:28:52 -0500

    Working on TIY in chapter 2

---
+++ python_work/tiy_chapter_2.py
@@ -0,0 +1,14 @@
+# 2-3 Use a variable to reprsent a name and print a message to that person
+name = "Eric"
+print(f'Hello {name}, would you like to learn some Python today?')
+
+# 2-4 Use a variable to represent a person's name and then print that name in all cases
+my_name = "clayton seabaugh"
+print(my_name.title())
+print(my_name.lower())
+print(my_name.upper())
+
+# 2-5 Find a quote, print the quote and name of author. 
+author = "René Descartes"
+quote = '"I think, therefore I am"'
+print(f'{author} once said, {quote}')
