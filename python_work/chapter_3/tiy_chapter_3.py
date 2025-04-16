cdf1302ed1dcca892bd94ff190117ba41a8aaff6
ClaytonSeabaughGH <claytonseabaugh@gmail.com>
2025-04-16 01:33:28 -0500

    Chapter 3: Motorcycle example

---
+++ python_work/chapter_3/tiy_chapter_3.py
@@ -0,0 +1,11 @@
+# 3.1: Store names of friends in a list
+names = ['Zach', 'Rachel', 'Cassie']
+
+# Print each person's name by accessing each element
+print(names[0])
+print(names[1])
+print(names[2])
+
+# Create a greeting for people in the list
+message = (f"Nice to meet you {names[0]}. My name is Clayton!")
+print(message)
