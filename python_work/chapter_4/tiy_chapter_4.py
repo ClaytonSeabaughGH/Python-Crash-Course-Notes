7d3951dc3d721917470da4d15ea18ed5505013e4
ClaytonSeabaughGH <claytonseabaugh@gmail.com>
2025-05-02 19:31:44 -0500

    Working on loops

--- python_work/chapter_4/tiy_chapter_4.py
+++ python_work/chapter_4/tiy_chapter_4.py
@@ -1,1 +1,11 @@
-# 4-1
+# 4-1 Pizzas
+pizzas = ["peperoni", "cheese", "sausage"]
+for pizza in pizzas:
+    print(f"I like {pizza} pizza.")
+print("I really love pizza!")
+
+# 4-2 Animals
+animals = ["dog", "cat", "rabbit"]
+for animal in animals:
+    print(f"A {animal} would make a great pet.")
+print("Any of these animals would be a great pet!")
